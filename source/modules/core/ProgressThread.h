ca33d0c94d7e46db1e75359d2ee493480951c24a
Sebastian <sebastian@friendlyshade.com>
2024-03-13 16:16:55 -0500

    Add missing license information and "../fsi_core_exports.h" include

--- source/modules/core/ProgressThread.h
+++ source/modules/core/ProgressThread.h
@@ -1,4 +1,13 @@
+// © 2023 Friendly Shade, Inc.
+// © 2023 Sebastian Zapata
+//
+// This file is part of FSI.
+// FSI is licensed under The MIT License. If a copy of The MIT License was not distributed with this
+// file, you can obtain one at https://opensource.org/license/mit.
+
 #pragma once
+
+#include "../fsi_core_exports.h"
 #include "../global.h"
 #include <thread>
 #include <functional>
